7b7efbda92b197bb40b394e88e11e34ac87afdd3
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-03 10:03:58 +0100

    adicionando o token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import {getRepository} from 'typeorm';
 import {compare} from 'bcryptjs';
+import {sign} from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request{
@@ -8,6 +10,7 @@ interface Request{
 }
 interface Response{
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService{
@@ -24,8 +27,14 @@ class AuthenticateUserService{
       throw new Error ('Incorrect email/password combination.');
     }
 
+    const token = sign({},'091acdd2becd63ac66a2afbcde829eb0' ,{
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return{
       user,
+      token,
     };
   }
 
