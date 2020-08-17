df99a294e2f0d7507ce7cf7447bae7073cab8b6e
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-17 20:07:28 +0100

    conceitos DDD

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,24 @@
+import { getRepository, Repository } from 'typeorm';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/appointments/dtos/ICreateUserDTO';
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async findById(id: string): Promise<User | undefined>;
+
+  public async create({ provider_id, date }: ICreateUserDTO): Promise<User> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default UsersRepository;
