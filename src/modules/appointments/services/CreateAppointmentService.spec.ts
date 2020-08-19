fa5908fd8ef3501e358ed2be1ae176d0521c707b
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-19 23:19:38 +0100

    criação do primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,8 +2,19 @@ import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRep
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
+  it('should be able to create a new appointment', async () => {
     const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
   });
 });
 
