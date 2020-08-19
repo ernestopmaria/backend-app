2033654b92b598477d1fbce255614ceaeda62a76
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-19 22:42:42 +0100

    criando um fake repository

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,12 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+  });
 });
+
+/* it('should note be able to create two appointment on the same time', () => {
+  expect(1 + 2).toBe(3);
+}); */
