d96916028b769e9f1aa8235b5d8fee93c788ff7e
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-08-23 23:13:30 +0100

    testando mesmo email

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
@@ -13,5 +14,24 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'Ernesto Maria',
+      email: 'ernestom@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Ernesto Maria',
+        email: 'ernestom@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
