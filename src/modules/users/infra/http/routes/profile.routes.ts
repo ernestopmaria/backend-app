b8bade861bd5574a1b08c6dc97412f7a927cdc54
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-09-02 22:09:58 +0100

    listagem de prestadores

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -8,7 +8,7 @@ const profileRouter = Router();
 const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticaded);
-
+profileRouter.get('/', profileController.show);
 profileRouter.put('/', profileController.update);
 
 export default profileRouter;
