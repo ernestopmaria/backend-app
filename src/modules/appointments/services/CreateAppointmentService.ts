323ce8b048d53cec18b0de30ec0b410f84ce5938
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-09-27 13:03:22 +0100

    cache

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import AppError from '@shared/errors/AppError';
 
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -20,6 +21,9 @@ class CreateAppointmentService {
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -60,6 +64,13 @@ class CreateAppointmentService {
       recipient_id: provider_id,
       content: `Novo Angendamento para o dia ${dateFormatted}`,
     });
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'YYYY-M-d',
+      )}`,
+    );
 
     return appointment;
   }
